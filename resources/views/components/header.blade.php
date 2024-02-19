ef90b6a015f2ae1575356168dce0998b0ac1aeaa
Rinaldito <rinalditoryanari@gmail.com>
2024-02-19 08:26:50 +0700

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -137,10 +137,10 @@
                     <span class="user-name">Ross C. Lopez</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                    <a class="dropdown-item" href="../page/profile.html"><i class="dw dw-user1"></i> Profile</a>
-                    <a class="dropdown-item" href="../page/profile.html"><i class="dw dw-settings2"></i> Setting</a>
-                    <a class="dropdown-item" href="../page/faq.html"><i class="dw dw-help"></i> Help</a>
-                    <a class="dropdown-item" href="../page/login.html"><i class="dw dw-logout"></i> Log Out</a>
+                    <a class="dropdown-item" href="{{url('/profile')}}"><i class="dw dw-user1"></i> Profile</a>
+                    <a class="dropdown-item" href="{{url('/profile')}}"><i class="dw dw-settings2"></i> Setting</a>
+                    <a class="dropdown-item" href="{{url('/faq')}}"><i class="dw dw-help"></i> Help</a>
+                    <a class="dropdown-item" href="{{url('/login')}}"><i class="dw dw-logout"></i> Log Out</a>
                 </div>
             </div>
         </div>
