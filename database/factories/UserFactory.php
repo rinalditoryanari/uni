5690e1a92c3120b5d3ab2c5e02b087517bd53d78
Rinaldito <rinalditoryanari@gmail.com>
2024-02-22 18:18:22 +0700

    added migration, seeder, factory for User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->numberBetween(0,3),
             'remember_token' => Str::random(10),
         ];
     }
